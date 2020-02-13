9136671f3f3e8b36442ef5a6eda6c3465d96c243
Andy Carr <andy@nextfriday.co.uk>
2020-02-13 12:12:23 +0000

    Moved to Lumpy & Bumpy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import Catalog from './Catalog';
+import Pictures from './Pictures';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">The Catalog App</h1>
+          <h1 className="App-title">Lumps and Bumps</h1>
         </header>
-        <Catalog />
+        <Pictures />
       </div>
     );
   }
